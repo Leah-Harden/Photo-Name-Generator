2aca0125fadf09f752243e43f52b0f4906050d37
Leah <leahxharden@gmail.com>
2023-02-19 19:10:23 -0500

    deleting comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 
-const nameButton = document.getElementById("nameBtn");
+let nameButton = document.getElementById("nameBtn");
 let name = nameButton.value;
 let Url = "https://leah-harden.github.io/What-the-Magic-Ball-see/"
-let completeUrl = Url + 
+let completeUrl = Url + ""
+
 $(nameButton).click(function(){
     preventDefault()
         function change_page(){
-            window.location.replace("page-two.html");
+            document.location.replace("./page-two.html");
         }; 
         change_page()
 
@@ -16,81 +17,3 @@ $(nameButton).click(function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const apikey = aeYvjbM5OcoeBJ6Pz3k70JlvcCifhD67kmBsFyRtkBGXhxQUTIGDCLE9
-// const nameButton = document.getElementById("inputName");
-
-// var guessedGender = ""
-
-// var guessedOrigin = ""
-
-// var guessedAge = ""
-
-
-
-
-// function predictAge(){
-//     //let inpName = document.getElementById("inputName");
-//     let name = nameButton.value;
-    
-//     fetch('https://api.agify.io?name='+ name,{ method:'GET'})
-//     .then(function(response){return response.json();})
-//     .then(data => {
-//         if (name != '') {
-//             guessedAge = data.age;
-//             displayAge = data.age;
-//             console.log(data.age);
-//         }else{
-//             console.log("broked age") ; 
-//         }
-        
-        
-//     })
-//     .catch(err => console.log(err));
-// }
-
-
-
-
-
-// $(nameButton).click(function(){
-//     predictAge()
-//     });
-
-
-// // function magicBall(){
-
-// //     import { createClient } from 'pexels';
-    
-// //     const client = createClient(apikey);
-// //     const query = guessedAge + "years old" + guessedOrigin + guessedGender;
-    
-// //     client.photos.search({ query, per_page: 1 }).then(photos => {...});
-// // }
