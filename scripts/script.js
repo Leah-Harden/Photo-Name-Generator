20accadf4ccc08ff0af582841fb08b6ac66d471f
Leah <leahxharden@gmail.com>
2023-02-19 19:21:02 -0500

    possible fix to page switch

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,10 @@ let name = nameButton.value;
 let Url = "https://leah-harden.github.io/What-the-Magic-Ball-see/"
 let completeUrl = Url + ""
 
-$(nameButton).click(function(){
-    preventDefault()
+$(nameButton).click(function(event){
+    event.preventDefault()
         function change_page(){
-            document.location.replace("./page-two.html");
+            document.location.pathname("page-two.html");
         }; 
         change_page()
 
